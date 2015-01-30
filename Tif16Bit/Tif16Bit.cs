57116a84f8aaa8b2f1c281e9f6b8892c0a98638b
Colton McEntee <cjmcentee@gmail.com>
2015-01-29 19:46:23 -0800

    Removed height/width reqs from 16bit tif reading

--- Tif16Bit/Tif16Bit.cs
+++ Tif16Bit/Tif16Bit.cs
@@ -2,6 +2,8 @@
 using Emgu.CV.Structure;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +12,28 @@ namespace TifReader
 {
     public class Tif16Bit
     {
-        public static Image<Gray, UInt16> ReadTif(String filePath, int imageWidth, int imageHeight) {
-            // Read as ummanaged object
-            IntPtr imP = CvInvoke.cvLoadImage(filePath, Emgu.CV.CvEnum.LOAD_IMAGE_TYPE.CV_LOAD_IMAGE_ANYDEPTH);
+        public static Image<Gray, UInt16> ReadTif(String filePath) {
+            using(FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                using(Image tif = Image.FromStream(stream, false, false))
+                {
+                    // This is only meant to open tif files, so we can expect width and height to be integers
+                    int width = (int) tif.PhysicalDimension.Width;
+                    int height = (int) tif.PhysicalDimension.Height;
 
-            // Copy to managed image object
-            Image<Gray, UInt16> image = new Image<Gray, UInt16>(imageWidth, imageHeight);
-            CvInvoke.cvCopy(imP, image, IntPtr.Zero);           
-            CvInvoke.cvReleaseImage(ref imP);
+                    // Read as ummanaged object
+                    IntPtr imP = CvInvoke.cvLoadImage(filePath, Emgu.CV.CvEnum.LOAD_IMAGE_TYPE.CV_LOAD_IMAGE_ANYDEPTH);
+
+                    // Copy to managed image object
+                    Image<Gray, UInt16> image = new Image<Gray, UInt16>(width, height);
+                    CvInvoke.cvCopy(imP, image, IntPtr.Zero);           
+                    CvInvoke.cvReleaseImage(ref imP);
             
-            return image;
+                    return image;
+                }
+            }
+            
+            
         }
     }
 }
